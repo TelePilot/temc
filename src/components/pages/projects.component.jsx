0154a0335009c00288b1b1ae029008de5bb13a43
4Mago <marcus.bjorsson@gmail.com>
2020-11-09 12:20:03 +0100

    adding projects page with sanity hopefully

--- src/components/pages/projects.component.jsx
+++ src/components/pages/projects.component.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
+import { ProjectsContext } from '../store/Project.context'
+import imageUrlBuilder from '@sanity/image-url'
+import sanityClient from '../Client'
 
+const builder = imageUrlBuilder(sanityClient)
+function urlFor(source) {
+	return builder.image(source)
+}
 const Container = styled.div`
     width: 100%;
     height: 100%;
@@ -12,11 +19,26 @@ const Grid = styled.div`
     gap: 10px;
 `
 
+
+
 const Projects = () => {
+    const { projects } = useContext(ProjectsContext)
+
     return (
         <Container>
             <Grid>
-
+			{project
+				? project.map((item, id) => (
+					<Card key={id}>
+							<ProjectImage
+								alt='website image'
+								src={urlFor(item.websiteImage).url()}
+								/>
+							<Title>{item.clientName}</Title>
+							<ProjectText>{item.description}</ProjectText>
+                            </Card>
+				  ))
+				  : null}
             </Grid>
         </Container>
     )
