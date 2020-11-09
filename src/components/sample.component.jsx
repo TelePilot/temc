2df881dbf20c78271e0aed00e7a2c86f8fb09632
blixt <teodor.blixt@gmail.com>
2020-11-09 12:46:28 +0100

    här är allt fel

--- src/components/sample.component.jsx
+++ src/components/sample.component.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
 import CTA from './cta.component'
 import { ProjectContext } from '../store/Project.context'
@@ -11,40 +11,33 @@ function urlFor(source) {
 }
 
 const Container = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 80%;
-    
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 80%;
 `
 const Image = styled.img`
-    width:40%;
-    height: auto;
+	width: 40%;
+	height: auto;
 `
-const TextContainer = styled.div`
-`
+const TextContainer = styled.div``
 const Text = styled.p``
 const Header = styled.h2``
 
-
-
 const Sample = () => {
-    const { project } = useContext(ProjectContext)
-    const sample = project[0]
-    console.log(sample)
-    return (
-            <Container>
-             <Image alt="client Image" src={urlFor(project[1].websiteImage).url()}/>
-              
-                <TextContainer>
-                    <Header>{project[0].clientName}</Header>
-                    <Text>{project[0].description}</Text>
-                    <CTA/>
-                </TextContainer>
-
-            </Container>
-
-    )
+	const { project, loaded } = useContext(ProjectContext)
+	const sample = project[0]
+	console.log(project)
+	return (
+		<Container>
+			<Image alt='client Image' src={urlFor(sample.websiteImage).url()} />
+			<TextContainer>
+				<Header>{sample.clientName}</Header>
+				<Text>{sample.description}</Text>
+				<CTA />
+			</TextContainer>
+		</Container>
+	)
 }
 
 export default Sample
--- src/store/Project.context.jsx
+++ src/store/Project.context.jsx
@@ -4,19 +4,23 @@ import sanityClient from '../Client'
 export const ProjectContext = createContext()
 
 const ProjectContextProvider = props => {
-	const [project, setProject] = useState([])
+	const [project, setProject] = useState('')
+	const [loaded, setLoaded] = useState(false)
 	useEffect(() => {
 		const projectQuery = `*[_type == "client"]`
-
+		console.log(projectQuery)
 		sanityClient.fetch(projectQuery).then(project => {
 			const projectArray = []
+			console.log(project)
 			project.forEach(project => {
 				projectArray.push(project)
 			})
 			setProject(projectArray)
+			setLoaded(true)
 		})
 		return
 	}, [])
+	console.log(project)
 	return (
 		<ProjectContext.Provider value={{ project }}>
 			{props.children}
