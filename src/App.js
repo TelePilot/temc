9d710c7bf6359d4597a1da3ae5f4034bc4748933
blixt <teodor.blixt@gmail.com>
2020-11-13 01:29:06 +0100

    scroll to top after animation finishes

--- src/App.js
+++ src/App.js
@@ -9,37 +9,44 @@ import Kontakt from './pages/kontakt.component'
 import ProjectExtended from './pages/project-extended.component'
 import { AnimatePresence } from 'framer-motion'
 import Home from './pages/home.component'
-import ScrollToTop from './components/func/scroll-to-top.component'
+
+const PageContainer = styled.div`
+	min-height: 110vh;
+`
 
 function App() {
-	console.log(AnimatePresence)
 	return (
 		<div className='App'>
-			{/* <ScrollToTop /> */}
 			<Navigation />
-			<Route
-				render={({ location }) => (
-					<AnimatePresence exitBeforeEnter initial={false}>
-						<Switch location={location} key={location.pathname}>
-							<Route exact path={'/'}>
-								<TaglineContextProvider>
+			<PageContainer>
+				<Route
+					render={({ location }) => (
+						<AnimatePresence
+							exitBeforeEnter
+							initial={false}
+							onExitComplete={() => window.scrollTo(0, 0)}
+						>
+							<Switch location={location} key={location.pathname}>
+								<Route exact path={'/'}>
+									<TaglineContextProvider>
+										<ProjectContextProvider>
+											<Home key={location.id} />
+										</ProjectContextProvider>
+									</TaglineContextProvider>
+								</Route>
+								<Route path={'/project/:id'}>
 									<ProjectContextProvider>
-										<Home key={location.id} />
+										<ProjectExtended key={location.id} />
 									</ProjectContextProvider>
-								</TaglineContextProvider>
-							</Route>
-							<Route path={'/project/:id'}>
-								<ProjectContextProvider>
-									<ProjectExtended key={location.id} />
-								</ProjectContextProvider>
-							</Route>
-							<Route exact path={'/kontakt'}>
-								<Kontakt key={location.id} />
-							</Route>
-						</Switch>
-					</AnimatePresence>
-				)}
-			/>
+								</Route>
+								<Route exact path={'/kontakt'}>
+									<Kontakt key={location.id} />
+								</Route>
+							</Switch>
+						</AnimatePresence>
+					)}
+				/>
+			</PageContainer>
 
 			<Footer />
 		</div>
