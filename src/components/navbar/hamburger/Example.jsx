11ad3ccdd1f34dec23270d2f6ef27cd453bdc743
blixt <teodor.blixt@gmail.com>
2020-11-17 15:19:31 +0100

    remove yarn

--- src/components/navbar/hamburger/Example.jsx
+++ src/components/navbar/hamburger/Example.jsx
@@ -1,80 +1,73 @@
-import React, {useRef} from "react";
-import { motion, useCycle } from "framer-motion";
-import  MenuToggle  from "./MenuToggle";
-import  Navigation  from "./Navigation";
+import React, { useRef } from 'react'
+import { motion, useCycle } from 'framer-motion'
+import MenuToggle from './MenuToggle'
+import Navigation from './Navigation'
 
 import styled from 'styled-components'
 
 const sidebar = {
-  open: () => ({
-    clipPath: `circle(${1000 * 2 + 200}px at 260px 40px)`,
+	open: () => ({
+		clipPath: `circle(${1000 * 2 + 200}px at 260px 40px)`,
 
-    transition: {
-      type: "spring",
-      stiffness: 20,
-      restDelta: 2,
-    }
-  }),
-  closed: {
-   
-    clipPath: "circle(30px at 260px 40px)",
-    transition: {
-      delay: 0.5,
-      type: "spring",
-      stiffness: 400,
-      damping: 40,
-    }
-  }
-};
+		transition: {
+			type: 'spring',
+			stiffness: 20,
+			restDelta: 2,
+		},
+	}),
+	closed: {
+		clipPath: 'circle(30px at 260px 40px)',
+		transition: {
+			delay: 0.5,
+			type: 'spring',
+			stiffness: 400,
+			damping: 40,
+		},
+	},
+}
 
 const Container = styled.div`
-position: absolute;
-  height: 100%;
-  z-index: 9;
+	position: absolute;
+	height: 100%;
+	z-index: 9;
 `
 
 const Nav = styled(motion.nav)`
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  width: 300px;
-  z-index: 999;
-
+	position: fixed;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	width: 300px;
+	z-index: 999;
 `
 
 const Background = styled(motion.div)`
-  position: absolute;
-  top: 0;
+	position: absolute;
+	top: 0;
 	right: 0;
 	bottom: 0;
-  width: 300px;
-  background: #fff;
-  opacity: 0.4;
-  
-    @media screen and (max-width: 700px) {
-      opacity: 0.7;
-    }
+	width: 300px;
+	background: #fff;
+	opacity: 0.4;
+
+	@media screen and (max-width: 700px) {
+		opacity: 0.7;
+	}
 `
 
 const Example = () => {
-  const [isOpen, toggleOpen] = useCycle(false, true);
-  const BackgroundToggle = useRef(null)
-  return (
-    <>
-    <Container  onClick={() => isOpen ? toggleOpen() : null}></Container>
-    <Nav
-   
-   initial={false}
-   animate={isOpen ? "open" : "closed"}>
-   <Background ref={BackgroundToggle} variants={sidebar} />
-   <Navigation  />
-   <MenuToggle toggle={() => toggleOpen()} />
- </Nav>
-  </>
-
-   
-  );
-};
+	const [isOpen, toggleOpen] = useCycle(false, true)
+	const BackgroundToggle = useRef(null)
+	return (
+		<>
+			<Container onClick={() => (isOpen ? toggleOpen() : null)}></Container>
+			<Nav initial={false} animate={isOpen ? 'open' : 'closed'}>
+				<Background ref={BackgroundToggle} variants={sidebar} />
+				<Navigation />
+				<MenuToggle toggle={() => toggleOpen()} />
+			</Nav>
+		</>
+	)
+}
 
 export default Example
