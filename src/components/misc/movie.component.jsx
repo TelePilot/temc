a1f7958dfd190ace1ff9ce1b02495c9fd5bb492a
4Mago <47850942+4Mago@users.noreply.github.com>
2020-11-18 18:07:24 +0100

    commitmtimti

--- src/components/misc/movie.component.jsx
+++ src/components/misc/movie.component.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Video = styled.video`
-	width: 60%;
-	height: auto;
+const Container = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+`
+
+const Video = styled.div`
 	margin-bottom: 10vh;
 	@media screen and (max-width: 700px) {
 		width: 100%;
 	}
 `
 
+
 const Movie = () => {
 	return (
-		<div>
-			<Video controls>
-				<source src='/media/SanityMovie.mp4' type='video/mp4' />
-			</Video>
-		</div>
+			<Container>
+		<Video>
+			<iframe width="1080" height="480" src="https://www.youtube.com/embed/bkBXiKwahMc?start=5" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</Video>
+	</Container>
 	)
 }
 
